634cf88d82c7d38f6c396eff9906d219ea6241cc
JKirchartz <jkirchartz@gmail.com>
2018-09-13 13:41:00 -0400

    clean up resume for printing - add function to limit number of posts to show (add a hash followed by the number to the end of the URL)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,40 +1,59 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
-	// Project configuration.
-	grunt.initConfig({
-		// Metadata.
-		"pkg": grunt.file.readJSON("package.json"),
-		// Task configuration.
-		"sass": {
-			docs : {
-				options : {
-					style: "expanded"
-				},
-				files: {
-					"docs/style.css": "src/style.scss"
-				}
-			}
-		},
-		"compile-handlebars": {
-			allStatic: {
-				files: [{
-					expand: true,
-					cwd: "src",
-					src: "index.hbs",
-					dest: "docs",
-					ext: ".html",
-				}],
-				globals: ["src/resume.json"]
-			}
-		},
-	});
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    "pkg": grunt.file.readJSON("package.json"),
+    // Task configuration.
+    "sass": {
+      docs : {
+        options : {
+          style: "expanded"
+        },
+        files: {
+          "docs/style.css": "src/style.scss"
+        }
+      }
+    },
+    "compile-handlebars": {
+      allStatic: {
+        files: [{
+          expand: true,
+          cwd: "src",
+          src: "index.hbs",
+          dest: "docs",
+          ext: ".html",
+        }],
+        globals: ["src/resume.json"]
+      }
+    },
+    watch: {
+      files: "src/**",
+      tasks: ["sass", "compile-handlebars"]
+    },
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src : [ "src/**" ]
+        },
+        options: {
+          watchTask: true,
+          open: false,
+          server: "./docs/"                 }
+      }
+    }
 
-	// These plugins provide necessary tasks.
-	grunt.loadNpmTasks("grunt-contrib-sass");
-	grunt.loadNpmTasks("grunt-compile-handlebars");
+  });
 
-	// Default task.
-	grunt.registerTask("default", ["compile-handlebars", "sass"]);
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks("grunt-contrib-sass");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-compile-handlebars");
+  grunt.loadNpmTasks("grunt-browser-sync");
+
+  // Default task.
+  grunt.registerTask("dev", ["browserSync", "watch"]);
+  grunt.registerTask("default", ["compile-handlebars", "sass"]);
 
 };
